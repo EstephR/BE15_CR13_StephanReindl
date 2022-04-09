4a76e6e8e96e5a36f911960218c6a21f5f785ed4
Stephan Reindl <stephan.reindl@gmx.at>
2022-04-09 02:23:56 +0200

    added

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,13 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Events;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('main/index.html.twig');
+        $events = $doctrine->getRepository(Events::class)->findAll();
+        return $this->render('main/index.html.twig', ['events'=> $events]);
     }
 
     #[Route('/create', name: 'app_create')]
@@ -20,15 +23,16 @@ class MainController extends AbstractController
         return $this->render('main/create.html.twig');
     }
 
-    #[Route('/details{id}', name: 'app_details')]
-    public function details($id): Response
+    #[Route('/details/{id}', name: 'main_details')]
+    public function details(ManagerRegistry $doctrine, $id): Response
     {
-        return $this->render('main/details.html.twig');
+      $event = $doctrine->getRepository(Events::class)->find($id);
+      return $this->render('main/details.html.twig', ['event' => $event]);
     }
 
-    #[Route('/edit{id}', name: 'app_edit')]
+    #[Route('/edit/{id}', name: 'main_edit')]
     public function edit($id): Response
     {
-        return $this->render('main/edit.html.twig');
+      return $this->render('main/edit.html.twig');
     }
 }
---
+++ src/Entity/Events.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventsRepository::class)]
+class Events
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $name;
+
+    #[ORM\Column(type: 'date')]
+    private $date;
+
+    #[ORM\Column(type: 'time')]
+    private $time;
+
+    #[ORM\Column(type: 'string', length: 250)]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $imagethumbnail;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $image;
+
+    #[ORM\Column(type: 'integer')]
+    private $capacity;
+
+    #[ORM\Column(type: 'string', length: 150)]
+    private $email;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $phone;
+
+    #[ORM\Column(type: 'string', length: 250)]
+    private $address;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $website;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getTime(): ?\DateTimeInterface
+    {
+        return $this->time;
+    }
+
+    public function setTime(\DateTimeInterface $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImagethumbnail(): ?string
+    {
+        return $this->imagethumbnail;
+    }
+
+    public function setImagethumbnail(string $imagethumbnail): self
+    {
+        $this->imagethumbnail = $imagethumbnail;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCapacity(): ?int
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(int $capacity): self
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getWebsite(): ?string
+    {
+        return $this->website;
+    }
+
+    public function setWebsite(string $website): self
+    {
+        $this->website = $website;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
